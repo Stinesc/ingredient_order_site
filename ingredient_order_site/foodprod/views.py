f67243fb7d2bdf7027598a8d8ed761891ef7d274
stinesc <limarevigor@gmail.com>
2019-02-12 16:49:14 +0200

    add list of dishes and search

--- ingredient_order_site/foodprod/views.py
+++ ingredient_order_site/foodprod/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.db.models import Q
+from .models import Dish
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "foodprod/index.html"
+    model = Dish
+
+    def get_queryset(self):
+        search_query = self.request.GET.get('search_query')
+        if search_query:
+            object_list = self.model.objects.filter(Q(name__icontains=search_query) |
+                                                    Q(description__icontains=search_query))
+        else:
+            object_list = self.model.objects.all()
+        return object_list
--- ingredient_order_site/ingredient_order_site/urls.py
+++ ingredient_order_site/ingredient_order_site/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('index', permanent=False)),
     path('admin/', admin.site.urls),
     path('foodprod/', include('foodprod.urls')),
 ]
